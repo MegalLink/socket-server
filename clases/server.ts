0bf195d4be19881ac5616d240ef52e728da563bb
MegalLink <50426219+MegalLink@users.noreply.github.com>
2020-04-19 23:29:52 -0500

    Configuracion de Usuario

--- clases/server.ts
+++ clases/server.ts
@@ -2,40 +2,41 @@ import express from "express";
 import { SERVER_PORT } from "../global/environment";
 import socketIO from "socket.io";
 import http from "http";
-import * as socket from '../sockets/socket';
+import * as socket from "../sockets/socket";
 export default class Server {
-
   public app: express.Application;
   public port: number;
   public io: socketIO.Server;
   private httpServer: http.Server;
-  private static _instance:Server;
+  private static _instance: Server;
 
   private constructor() {
-    
     this.app = express();
     this.port = SERVER_PORT;
     this.httpServer = new http.Server(this.app);
     this.io = socketIO(this.httpServer);
     this.escucharSockets();
   }
-  public static get instance(){
-      //si ya existe una instancia regresa la instancia si no existe cre una instancia
-        return this._instance || (this._instance=new this());
+  public static get instance() {
+    //si ya existe una instancia regresa la instancia si no existe cre una instancia
+    return this._instance || (this._instance = new this());
   }
   private escucharSockets() {
-
     //Aqui escucho lo que el cliente emite
     console.log("Escuchando conexiones - sockets");
 
-
     //on es para escuchar
     this.io.on("connection", (cliente) => {
-      console.log("Cliente conectado");
-        //Mensajes
-        socket.mensaje(cliente,this.io);
-        //Desconectar
-        socket.desconectar(cliente);
+      //Conectar Cliente
+      socket.conectarCliente(cliente);
+
+
+    //Configurar Usuario
+    socket.configurarUsuario(cliente, this.io);
+      //Mensajes
+      socket.mensaje(cliente, this.io);
+      //Desconectar
+      socket.desconectar(cliente);
       
     });
   }
--- sockets/socket.ts
+++ sockets/socket.ts
@@ -1,13 +1,31 @@
 import { Socket } from "socket.io";
+import { UsuariosLista } from "../clases/usuarios-lista";
+import { Usuario } from "../clases/usuario";
+export const usuariosConectados=new UsuariosLista();
+
+export const conectarCliente=(cliente:Socket)=>{
+    const ususario=new Usuario(cliente.id);
+    usuariosConectados.agregarUsuario(ususario);
+}
 
 export const desconectar=(cliente:Socket)=>{
     cliente.on('disconnect',()=>{
-        console.log("Cliente desconectado")
+        const temp=usuariosConectados.borrarUsuario(cliente.id)
+       console.log("USUARIO BORRADO:",temp)
     });
 }
 export const mensaje=(cliente:Socket,io:SocketIO.Server)=>{
     cliente.on('mensaje',(payload:{de: string, cuerpo:string})=>{
                 console.log("Mensaje recibido", payload);
                     io.emit('mensaje-nuevo',payload);
+    });
+}
+export const configurarUsuario=(cliente:Socket,io:SocketIO.Server)=>{
+    cliente.on('configurar-usuario',(payload:{nombre:string},callback:Function)=>{
+        usuariosConectados.actualizarNombre(cliente.id,payload.nombre);
+        callback({
+            ok:true,
+            mensaje:`Usuario ${payload.nombre} configurado`
+        })
     });
 }
